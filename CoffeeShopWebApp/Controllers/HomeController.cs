134d0b2d05d61fbbe696ddd26f661d8e48ee6bb7
Jason Kubo <kuboj86@gmail.com>
2017-11-17 08:49:37 -0500

    first commit

---
+++ CoffeeShopWebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShopWebApp.Models;
+
+namespace CoffeeShopWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View(); 
+        }
+
+        public ActionResult NewUserRegistered(UserInput NewCustomer)
+        {
+            ViewBag.FirstName = NewCustomer.FirstName;
+            return View();
+        }
+    
+    }
+}
---
+++ CoffeeShopWebApp/Models/UserInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopWebApp.Models
+{
+    public class UserInput
+    {
+        public UserInput()
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            TelephoneNumber = 0;
+            Password = "";
+        }
+
+        public UserInput(string firstname, string lastname, string email, int telephonenumber, string password)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Email = email;
+            this.TelephoneNumber = telephonenumber;
+            this.Password = password;
+        }
+        [Required]
+        public string FirstName { set; get; }
+        [Required]
+        public string LastName { set; get; }
+        [Required]
+        public string Email { set; get; }
+        [Required]
+        public int TelephoneNumber { set; get; }
+        [Required]
+        public string Password { set; get; }
+        
+            
+        
+    }
+}
